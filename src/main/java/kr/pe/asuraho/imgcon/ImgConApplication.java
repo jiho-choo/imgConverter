5ffb2518f3655b4ff10c35f8c5096a18aadd396b
jiho-choo <asuraho@gmail.com>
2024-05-16 20:47:10 +0900

    first

---
+++ src/main/java/kr/pe/asuraho/imgcon/ImgConApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.asuraho.imgcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgConApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgConApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/pe/asuraho/imgcon/ImgConApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.asuraho.imgcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgConApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
